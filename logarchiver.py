4408fa2dc292fe3495079ef042e26f69ef897833
Tom Robertson <tommertron@gmail.com>
2021-12-27 13:15:06 -0500

    Adding logarchivery and datearchiver to repository

---
+++ logarchiver.py
@@ -0,0 +1,12 @@
+import os
+from datetime import datetime
+import shutil
+
+now = datetime.now()
+archiveDate = now.strftime("%m-%d-%y")
+newname = "/logarchive-"+archiveDate+".txt"
+thepath = (os.getcwd())
+
+os.rename(thepath+'/log.txt',thepath+newname)
+shutil.move(thepath+newname,thepath+'/LogArchive'+newname)
+f = open('log.txt', "x")
