91f7fef72e5021172f36072fe3bae4e30d5e9969
waffles <petermblair@gmail.com>
2022-01-03 20:07:06 -0800

    Ran `black` on all python files in directory

--- datearchiver.py
+++ datearchiver.py
@@ -1,6 +1,6 @@
 import sys
 
-with open('dates.txt','r') as f:
-	data = f.read().splitlines(True)
-with open('dates.txt', 'w') as fout:
+with open("dates.txt", "r") as f:
+    data = f.read().splitlines(True)
+with open("dates.txt", "w") as fout:
     fout.writelines(data[1:])
