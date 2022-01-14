b968a75006328144ce2e8a06b765fc870a517d76
waffle2k <petermblair@gmail.com>
2022-01-13 21:54:58 -0800

    Start moving ontario govt data sources to classes with tests (#30)
    * Adding files
    
    * Ran black
    
    * Records are stored to CSV
    
    * fixed tests
    
    * One last test tweak
    
    * weird
    
    * fixed typo
    
    * Removed unnecessary object

--- datasources/ontgov.py
+++ datasources/ontgov.py
@@ -1,6 +1,134 @@
 #!/usr/bin/env python3
 
+from datetime import datetime, timedelta, date
+import json
+import csv
+import urllib.request as ur
+import urllib.parse as prs
 
-def query():
-    """Query the ontario govt http endpoint"""
-    raise NotImplementedError()
+
+class DateRanges:
+    """Generate a list of 10 datetime objects starting from today"""
+
+    @staticmethod
+    def range(size: int = 10):
+        return [DateRanges._daysago(i) for i in range(size)]
+
+    @staticmethod
+    def _daysago(days: int) -> datetime:
+        return date.today() - timedelta(days=days)
+
+
+class OntarioGov:
+    DATASOURCES = {
+        "CaseData": {
+            "id": "ed270bb8-340b-41f9-a7c6-e8ef587e6d11",
+            "datename": "Reported Date",
+            "fields": [
+                "Total Cases",
+                "Number of patients hospitalized with COVID-19",
+                "Number of patients in ICU due to COVID-19",
+            ],
+        },
+        "VaccineData": {
+            "id": "8a89caa9-511c-4568-af89-7f2174b4378c",
+            "datename": "report_date",
+            "fields": [
+                "total_doses_administered",
+                "total_individuals_at_least_one",
+                "total_individuals_fully_vaccinated",
+                "total_individuals_3doses",
+            ],
+        },
+    }
+
+    def __init__(self):
+        self.map = {}
+
+    def query(self, date: datetime, dataset: str, qfields):
+        """Query the ontario govt http endpoint"""
+        urlstart = "https://data.ontario.ca/api/3/action/datastore_search?"
+        resourceid = "resource_id=" + OntarioGov.DATASOURCES[dataset]["id"]
+        fieldQuery = "fields=%s" % (",".join(qfields))
+        reqdate = (
+            'filters={"'
+            + OntarioGov.DATASOURCES[dataset]["datename"]
+            + '":["'
+            + str(date)
+            + '"]}'
+        )
+        queryurl = urlstart + resourceid + "&" + fieldQuery + "&" + reqdate
+        queryurl = queryurl.replace(" ", "%20")
+        fileobj = ur.urlopen(queryurl)
+        return json.loads(fileobj.read())
+
+    def _cleanup(self, og_results):
+        if not og_results["success"]:
+            raise Exception("Failed to pull data from OntarioGov")
+        results = og_results["result"]["records"][0]
+        results.update((k, int(v)) for k, v in results.items())
+        return results
+
+    def vaccinedata(self, date: datetime):
+        return self._cleanup(
+            self.query(
+                date, "VaccineData", OntarioGov.DATASOURCES["VaccineData"]["fields"]
+            )
+        )
+
+    def casedata(self, date: datetime):
+        return self._cleanup(
+            self.query(date, "CaseData", OntarioGov.DATASOURCES["CaseData"]["fields"])
+        )
+
+    def get(self, date: datetime):
+        self.map[str(date)] = self.vaccinedata(date)
+        self.map[str(date)].update(self.casedata(date))
+        self.map[str(date)].update({"date": str(date)})
+        return self.map[str(date)]
+
+
+class CovidRecords(OntarioGov):
+    FIELDNAMES = [
+        "date",
+        "total_doses_administered",
+        "total_individuals_at_least_one",
+        "total_individuals_fully_vaccinated",
+        "total_individuals_3doses",
+        "Total Cases",
+        "Number of patients hospitalized with COVID-19",
+        "Number of patients in ICU due to COVID-19",
+    ]
+
+    def __init__(self, filename="foo.csv"):
+        super().__init__()
+        self.filename = filename
+        try:
+            with open(filename, newline="") as csvfile:
+                reader = csv.DictReader(csvfile)
+                for row in reader:
+                    date = row["date"]
+                    self.map[date] = row
+        except FileNotFoundError:
+            self.map = {}
+
+    def get(self, date: datetime):
+        if str(date) not in self.map:
+            return super().get(date)
+        return self.map[str(date)]
+
+    def write(self):
+        with open(self.filename, "w", newline="") as csvfile:
+            writer = csv.DictWriter(csvfile, CovidRecords.FIELDNAMES)
+            writer.writeheader()
+            for d in sorted(self.map.keys(), reverse=True):
+                writer.writerow(self.map[d])
+
+    def backfill(self, n=10):
+        [self.get(date) for date in DateRanges.range(n)]
+
+
+if __name__ == "__main__":
+    records = CovidRecords()
+    records.backfill()
+    records.write()
--- tests/test_ontariodatasource.py
+++ tests/test_ontariodatasource.py
@@ -1,14 +1,55 @@
 #!/usr/bin/env python3
 
 import unittest
-
+import datetime
+import json
+from unittest.mock import MagicMock, patch
 from datasources import ontgov
+
+expected_results = """{"help": "https://data.ontario.ca/api/3/action/help_show?name=datastore_search", "success": true, "result": {"include_total": true, "resource_id": "8a89caa9-511c-4568-af89-7f2174b4378c", "fields": [{"type": "int", "id": "_id"}, {"info": {"notes": "", "type_override": "timestamp", "label": ""}, "type": "timestamp", "id": "report_date"}, {"info": {"notes": "", "type_override": "numeric", "label": ""}, "type": "numeric", "id": "previous_day_total_doses_administered"}, {"info": {"notes": "", "type_override": "numeric", "label": ""}, "type": "numeric", "id": "previous_day_at_least_one"}, {"info": {"notes": "", "type_override": "numeric", "label": ""}, "type": "numeric", "id": "previous_day_fully_vaccinated"}, {"type": "text", "id": "previous_day_3doses"}, {"info": {"notes": "", "type_override": "numeric", "label": ""}, "type": "numeric", "id": "total_doses_administered"}, {"type": "text", "id": "total_individuals_at_least_one"}, {"type": "text", "id": "total_individuals_partially_vaccinated"}, {"type": "text", "id": "total_doses_in_fully_vaccinated_individuals"}, {"type": "text", "id": "total_individuals_fully_vaccinated"}, {"type": "text", "id": "total_individuals_3doses"}], "records_format": "objects", "records": [{"total_doses_administered": 28853124, "total_individuals_at_least_one": "12307610", "total_individuals_fully_vaccinated": "11493087", "total_individuals_3doses": "5033258"}], "_links": {"start": "/api/3/action/datastore_search?fields=total_doses_administered%2Ctotal_individuals_at_least_one%2Ctotal_individuals_fully_vaccinated%2Ctotal_individuals_3doses&filters=%7B%22report_date%22%3A%5B%222022-01-12%22%5D%7D&resource_id=8a89caa9-511c-4568-af89-7f2174b4378c", "next": "/api/3/action/datastore_search?fields=total_doses_administered%2Ctotal_individuals_at_least_one%2Ctotal_individuals_fully_vaccinated%2Ctotal_individuals_3doses&offset=100&filters=%7B%22report_date%22%3A%5B%222022-01-12%22%5D%7D&resource_id=8a89caa9-511c-4568-af89-7f2174b4378c"}, "filters": {"report_date": ["2022-01-12"]}, "total": 1}}"""
+
+
+class MockedDate(datetime.date):
+    @classmethod
+    def today(cls):
+        return cls(2022, 1, 12)
+
+
+datetime.date = MockedDate
 
 
 class TestOntarioDataSource(unittest.TestCase):
-    def test_call(self):
-        with self.assertRaises(NotImplementedError):
-            ontgov.query()
+    def test_query_vaccinedata(self):
+        with patch.object(
+            ontgov.OntarioGov, "query", return_value=json.loads(expected_results)
+        ) as mock_method:
+            og = ontgov.OntarioGov()
+            results = og.vaccinedata(datetime.date.today())
+        print(results)
+        mock_method.assert_called_with(
+            datetime.date(2022, 1, 12),
+            "VaccineData",
+            [
+                "total_doses_administered",
+                "total_individuals_at_least_one",
+                "total_individuals_fully_vaccinated",
+                "total_individuals_3doses",
+            ],
+        )
+        self.assertEqual(results["total_doses_administered"], 28853124)
+        self.assertEqual(results["total_individuals_at_least_one"], 12307610)
+        self.assertEqual(results["total_individuals_fully_vaccinated"], 11493087)
+        self.assertEqual(results["total_individuals_3doses"], 5033258)
+
+
+class TestDateRanges(unittest.TestCase):
+    def test_daterange_ten(self):
+        dates = ontgov.DateRanges.range()
+        self.assertEqual(len(dates), 10)
+
+    def test_daterange_five(self):
+        dates = ontgov.DateRanges.range(5)
+        self.assertEqual(len(dates), 5)
 
 
 if __name__ == "__main__":
